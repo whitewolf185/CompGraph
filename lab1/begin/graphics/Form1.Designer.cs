3e8939eeb83e2401931d1039ba53af04d017c596
Матвей <whitewolf.mot185@gmail.com>
2021-09-22 20:07:05 +0300

    lab1_ready2.0

---
+++ lab1/begin/graphics/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.Remoting.Channels;
+
+namespace graphics{
+    partial class Form1{
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing){
+            if (disposing && (components != null)){
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent(){
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.settings = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.num_Angle = new System.Windows.Forms.NumericUpDown();
+            this.labelScale = new System.Windows.Forms.Label();
+            this.numScale = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.parameter = new System.Windows.Forms.NumericUpDown();
+            this.step = new System.Windows.Forms.Label();
+            this.panelGraph = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.settings.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.num_Angle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numScale)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.parameter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 36);
+            this.numericUpDown1.Minimum = new decimal(new int[]{ 4, 0, 0, 0 });
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(74, 25);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[]{ 40, 0, 0, 0 });
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numeric_ValueChanged);
+            // 
+            // settings
+            // 
+            this.settings.Controls.Add(this.label5);
+            this.settings.Controls.Add(this.num_Angle);
+            this.settings.Controls.Add(this.labelScale);
+            this.settings.Controls.Add(this.numScale);
+            this.settings.Controls.Add(this.label1);
+            this.settings.Controls.Add(this.parameter);
+            this.settings.Controls.Add(this.step);
+            this.settings.Controls.Add(this.numericUpDown1);
+            this.settings.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.settings.Font = new System.Drawing.Font("Mongolian Baiti", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.settings.Location = new System.Drawing.Point(31, 28);
+            this.settings.Name = "settings";
+            this.settings.Size = new System.Drawing.Size(197, 139);
+            this.settings.TabIndex = 1;
+            this.settings.TabStop = false;
+            this.settings.Text = "Настройки";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(107, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 18);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "angle";
+            // 
+            // num_Angle
+            // 
+            this.num_Angle.Location = new System.Drawing.Point(109, 102);
+            this.num_Angle.Maximum = new decimal(new int[]{ 180, 0, 0, 0 });
+            this.num_Angle.Minimum = new decimal(new int[]{ 180, 0, 0, -2147483648 });
+            this.num_Angle.Name = "num_Angle";
+            this.num_Angle.Size = new System.Drawing.Size(72, 25);
+            this.num_Angle.TabIndex = 11;
+            this.num_Angle.ValueChanged += new System.EventHandler(this.num_Angle_ValueChanged);
+            // 
+            // labelScale
+            // 
+            this.labelScale.Location = new System.Drawing.Point(12, 84);
+            this.labelScale.Name = "labelScale";
+            this.labelScale.Size = new System.Drawing.Size(54, 18);
+            this.labelScale.TabIndex = 5;
+            this.labelScale.Text = "scale";
+            // 
+            // numScale
+            // 
+            this.numScale.DecimalPlaces = 3;
+            this.numScale.Increment = new decimal(new int[]{ 15, 0, 0, 0 });
+            this.numScale.Location = new System.Drawing.Point(12, 102);
+            this.numScale.Maximum = new decimal(new int[]{ 100000, 0, 0, 0 });
+            this.numScale.Minimum = new decimal(new int[]{ 10, 0, 0, 0 });
+            this.numScale.Name = "numScale";
+            this.numScale.Size = new System.Drawing.Size(73, 25);
+            this.numScale.TabIndex = 4;
+            this.numScale.Value = new decimal(new int[]{ 10, 0, 0, 0 });
+            this.numScale.ValueChanged += new System.EventHandler(this.numScale_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(109, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Параметр А";
+            // 
+            // parameter
+            // 
+            this.parameter.Location = new System.Drawing.Point(113, 36);
+            this.parameter.Maximum = new decimal(new int[]{ 20, 0, 0, 0 });
+            this.parameter.Minimum = new decimal(new int[]{ 20, 0, 0, -2147483648 });
+            this.parameter.Name = "parameter";
+            this.parameter.Size = new System.Drawing.Size(70, 25);
+            this.parameter.TabIndex = 2;
+            this.parameter.Value = new decimal(new int[]{ 5, 0, 0, 0 });
+            this.parameter.ValueChanged += new System.EventHandler(this.numeric_ValueChanged);
+            // 
+            // step
+            // 
+            this.step.Location = new System.Drawing.Point(12, 16);
+            this.step.Name = "step";
+            this.step.Size = new System.Drawing.Size(31, 17);
+            this.step.TabIndex = 1;
+            this.step.Text = "Шаг";
+            // 
+            // panelGraph
+            // 
+            this.panelGraph.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(230)))), ((int)(((byte)(233)))));
+            this.panelGraph.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelGraph.Location = new System.Drawing.Point(227, 64);
+            this.panelGraph.Margin = new System.Windows.Forms.Padding(10);
+            this.panelGraph.Name = "panelGraph";
+            this.panelGraph.Size = new System.Drawing.Size(821, 648);
+            this.panelGraph.TabIndex = 2;
+            this.panelGraph.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel_Paint);
+            this.panelGraph.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelGraph_MouseMove);
+            this.panelGraph.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelGraph_MouseUp);
+            this.panelGraph.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.panelGraph_WheelEvent);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(1058, 720);
+            this.Controls.Add(this.panelGraph);
+            this.Controls.Add(this.settings);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(600, 300);
+            this.Name = "Form1";
+            this.Text = "Визуализатор графика";
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.settings.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.num_Angle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numScale)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.parameter)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.NumericUpDown num_Angle;
+        private System.Windows.Forms.NumericUpDown numScale;
+
+        private System.Windows.Forms.Label labelScale;
+
+        private System.Windows.Forms.NumericUpDown parameter;
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.GroupBox settings;
+
+        private System.Windows.Forms.Panel panelGraph;
+
+        private System.Windows.Forms.Label step;
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+
+        #endregion
+    }
+}
---
+++ lab1/begin/graphics/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace graphics{
+    public partial class Form1 : Form{
+        public Form1(){
+            InitializeComponent();
+            Set_parameter((float) parameter.Value);
+            _step = 100;
+            Change_shifts();
+            Change_border();
+            diffX = 0;
+            diffY = 0;
+            compressingScale = 1;
+            originalScale = 50;
+            Set_scale();
+        }
+
+        #region MyLogic
+
+        #region Atributs
+
+        private float _a;
+        private PointF[] pointFs;
+        private int _step;
+        private int shiftX;
+        private int shiftY;
+        private float scale;
+        private int borderHeight;
+        private int borderWidth;
+        private bool wasMaximized = false;
+        private int diffX;
+        private int diffY;
+        private int absX;
+        private int absY;
+        private float sizeX;
+        private float sizeY;
+        private float compressingScale;
+        private float originalScale;
+        private bool beginDown = false;
+
+        #endregion
+        
+        private float X(float t){
+            return (_a * (float)Math.Cos(t)) / (1 + (float)Math.Sin(t) * (float)Math.Sin(t));
+        }
+
+        private float Y(float t){
+            return (_a * (float)Math.Cos(t) * (float)Math.Sin(t)) / (1 + (float)Math.Sin(t) * (float)Math.Sin(t));
+        }
+
+        #region gets
+
+        public void SetA(float _A){
+            _a = _A;
+        }
+
+        #endregion
+
+        #region sets
+
+        private void Change_shifts(){
+            shiftX = this.panelGraph.Width / 2 + absX;
+            shiftY = this.panelGraph.Height / 2 + absY;
+        }
+
+        private void Change_border(){
+            borderHeight = panelGraph.Height;
+            borderWidth = panelGraph.Width;
+        }
+
+        private void Set_parameter(float value){
+            _a = value;
+        }
+        
+        private void Set_MaxAndMin(){
+            float maxX = -10000;
+            float maxY = -10000;
+            float minX = 10000;
+            float minY = 10000;
+            
+            foreach (PointF pointF in pointFs){
+                minX = Math.Min(minX, pointF.X);
+                minY = Math.Min(minY, pointF.Y);
+                maxX = Math.Max(maxX, pointF.X);
+                maxY = Math.Max(maxY, pointF.Y);
+            }
+
+            sizeX = maxX - minX;
+            sizeY = maxY - minY;
+        }
+
+        private void Set_scale(){
+            scale = originalScale * compressingScale;
+        }
+
+        #endregion
+        
+        public void Generate_points(){
+            List<PointF> pointS = new List<PointF>();
+            float t;
+            float l = 0;
+            float r = 2 * (float)Math.PI;
+            
+            for (t = 0; t < r; t += (r - l)/_step){
+                pointS.Add(new PointF(X(t),Y(t)) );
+            }
+
+            Matrix transformMatrix = new Matrix();
+            var angle = (int)num_Angle.Value;
+            transformMatrix.Scale(scale, scale, MatrixOrder.Append);
+            transformMatrix.Rotate(angle);
+            transformMatrix.Translate(shiftX, shiftY, MatrixOrder.Append);
+
+            pointFs = pointS.ToArray();
+
+            transformMatrix.TransformPoints(pointFs);
+        }
+        
+        private void Pait_graph(PaintEventArgs e){
+            Change_shifts();
+            
+            _step = (int)this.numericUpDown1.Value;
+            Set_parameter((float) parameter.Value);
+            Generate_points();
+            PointF pref = pointFs[pointFs.Length - 1];
+
+            Pen pen = new Pen(Color.Tomato, 3);
+            
+            foreach (PointF pointF in pointFs){
+                e.Graphics.DrawLine(pen,pointF, pref);
+                pref = pointF;
+            }
+        }
+        
+        private void Create_Coordinates(PaintEventArgs e){
+            Pen coordinatePen = new Pen(Color.Black, 2);
+            Change_border();
+
+            PointF y_line1 = new PointF(shiftX, 0);
+            PointF y_line2 = new PointF(shiftX, borderHeight);
+
+            PointF x_line1 = new PointF(0, shiftY);
+            PointF x_line2 = new PointF(borderWidth, shiftY);
+
+            List<PointF> Singlers = new List<PointF>();
+
+            for (float i = shiftY; i >= 0; i -= scale){
+                Singlers.Add(new PointF(shiftX - 5,i));
+                Singlers.Add(new PointF(shiftX + 5,i));
+            }
+            
+            for (float i = shiftY; i < borderHeight ; i += scale){
+                Singlers.Add(new PointF(shiftX - 5,i));
+                Singlers.Add(new PointF(shiftX + 5,i));
+            }
+            
+            for (float i = shiftX; i >= 0; i -= scale){
+                Singlers.Add(new PointF(i,shiftY - 5));
+                Singlers.Add(new PointF(i,shiftY + 5));
+            }
+            
+            for (float i = shiftX; i < borderWidth ; i += scale){
+                Singlers.Add(new PointF(i,shiftY - 5));
+                Singlers.Add(new PointF(i,shiftY + 5));
+            }
+
+            e.Graphics.DrawLine(coordinatePen, y_line1, y_line2);
+            e.Graphics.DrawLine(coordinatePen, x_line1, x_line2);
+
+            for (int i = 0; i < Singlers.Count; i += 2){
+                e.Graphics.DrawLine(coordinatePen, Singlers[i], Singlers[i+1]);
+            }
+        }
+
+        
+
+        #endregion
+
+        private void Panel_Paint(object sender, PaintEventArgs e){
+            // throw new System.NotImplementedException();
+            Create_Coordinates(e);
+
+            Pait_graph(e);
+        }
+
+
+        private void Form1_Resize(object sender, EventArgs e){
+            this.panelGraph.Height = this.ClientSize.Height - panelGraph.Location.Y - panelGraph.Margin.All;
+            this.panelGraph.Width = this.ClientSize.Width - panelGraph.Location.X - panelGraph.Margin.All;
+            
+            Set_MaxAndMin();
+
+            if (sizeX > panelGraph.Width){
+                compressingScale *= panelGraph.Width  / (sizeX + 10);
+            }
+            
+            if (sizeY > panelGraph.Height){
+                compressingScale *= panelGraph.Height  / (sizeY + 10);
+            }
+
+            if ((sizeX < panelGraph.Width && originalScale < panelGraph.Width) || (sizeY < panelGraph.Height && originalScale < panelGraph.Height)){
+                compressingScale *= Math.Min(panelGraph.Width/ (sizeX + 10),panelGraph.Height/ (sizeY + 10) );
+                if (compressingScale > 1){
+                    compressingScale = 1;
+                }
+            }
+            
+            /*if (sizeY < panelGraph.Height && originalScale < panelGraph.Height){
+                compressingScale *= panelGraph.Height / (sizeY + 30);
+                if (compressingScale > 1){
+                    compressingScale = 1;
+                }
+            }*/
+            
+            Set_scale();
+
+            if (WindowState == FormWindowState.Maximized){
+                if (!wasMaximized){
+                    Refresh();
+                }
+
+                wasMaximized = true;
+            }
+
+            if (WindowState == FormWindowState.Normal ){
+                if (wasMaximized){
+                    Refresh();
+                }
+                wasMaximized = false;
+            }
+
+            Refresh();
+        }
+
+        private void panelGraph_WheelEvent(object sender, System.Windows.Forms.MouseEventArgs e){
+            var prev = originalScale;
+            var wheel = e.Delta * SystemInformation.MouseWheelScrollLines / 90;
+            var tmp = originalScale + wheel;
+
+            if (tmp >= 10){
+                originalScale = tmp;
+                Set_MaxAndMin();
+                if (sizeX > panelGraph.Width || sizeY > panelGraph.Height ){
+                    originalScale = prev - 10;
+                }
+                Set_scale();
+                
+                Refresh();
+            }
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e){
+            Refresh();
+        }
+
+
+        private void numeric_ValueChanged(object sender, EventArgs e){
+            Refresh();
+        }
+
+        private void numScale_ValueChanged(object sender, EventArgs e){
+            originalScale = float.Parse(numScale.Text);
+            Set_scale();
+            Refresh();
+        }
+
+
+        private void panelGraph_MouseMove(object sender, MouseEventArgs e){
+
+            if (e.Button == MouseButtons.Left){
+                if (!beginDown){
+                    diffX = e.Location.X;
+                    diffY = e.Location.Y;
+                    beginDown = true;
+                }
+                else{
+                    absX += e.Location.X - diffX;
+                    absY += e.Location.Y - diffY;
+                    diffX = e.Location.X;
+                    diffY = e.Location.Y;
+                    Refresh();
+                }
+            }
+            else{
+                beginDown = false;
+            }
+        }
+
+        private void num_Angle_ValueChanged(object sender, EventArgs e){
+            Refresh();
+        }
+
+        private void panelGraph_MouseUp(object sender, MouseEventArgs e){
+            Refresh();
+        }
+    }
+}
